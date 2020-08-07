2bc3903b66258a852b2d0e39d95c2b8934ed65fc
lytyazla <48278370+lytyazla@users.noreply.github.com>
2020-08-07 16:43:28 +0900

    完成

--- src/main/java/shopdate/Jdbc.java
+++ src/main/java/shopdate/Jdbc.java
@@ -1,5 +1,54 @@
 package shopdate;
 
-public class Jdbc {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
+public class Jdbc {
+	
+	Connection conn = null;
+	Statement stmt = null;
+	ResultSet rs = null;
+
+	String DB_URL = "jdbc:postgresql://localhost:5432/chen";
+	String USER = "postgres";
+	String PASS = "postgres";
+
+	public Connection getDb() throws ClassNotFoundException {
+		Class.forName("org.postgresql.Driver");
+		if (conn == null) {
+			try {
+				conn = DriverManager.getConnection(DB_URL, USER, PASS);
+				stmt = conn.createStatement();
+			} catch (SQLException ex) {
+				Logger.getLogger(Jdbc.class.getName()).log(Level.SEVERE, null, ex);
+			}
+		}
+		return conn;
+	}
+
+	public void closeDb() throws SQLException {
+		if (rs != null) {
+			rs.close();
+		}
+		if (stmt != null) {
+			stmt.close();
+		}
+		if (conn != null) {
+			conn.close();
+		}
+	}
+
+	public void insertShopDate(Shop sp) throws SQLException {
+		String sql = "insert into t_shopinfo(name,tel,address)values('";
+		sql += sp.getName() + "','";
+		sql += sp.getTel() + "','";
+		sql += sp.getAddress() + "')";
+		System.out.println(sql);
+		stmt.executeUpdate(sql);
+	}
 }
--- src/main/java/shopdate/ShopDate.java
+++ src/main/java/shopdate/ShopDate.java
@@ -3,6 +3,9 @@ package shopdate;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -11,18 +14,43 @@ import org.jsoup.select.Elements;
 
 public class ShopDate {
 	public static void main(String[] args) throws MalformedURLException, IOException {
+		List<String> list = new ArrayList<>();
 		String url = "https://tabelog.com/rstLst/washoku/";
-		Document document = Jsoup.parse(new URL(url), 3000);
-		Elements element = document.getElementsByClass("js-rstlist-info rstlist-info");
-
-		Elements elements = element.tagName("list-rst__rst-name-target cpy-rst-name");
-
+		Document document = Jsoup.parse(new URL(url), 30000);
+		Elements elements = document.getElementsByClass("list-rst__rst-name-target cpy-rst-name");
 		for (Element el : elements) {
-			String url2 = el.getElementsByTag("a").eq(0).attr("href");
+			String url2 = el.getElementsByClass("list-rst__rst-name-target cpy-rst-name").eq(0).attr("href");
 			Document document2 = Jsoup.parse(new URL(url2), 3000);
-			Elements element2 = document.getElementsByClass("rstinfo-table");
-			String shopName = element2.tagName("td").eq(1).text();
-			System.out.println(shopName);
+			Element element2 = document2.getElementById("contents-rstdata");
+			Elements elements2 = element2.getElementsByClass("c-table c-table--form rstinfo-table__table").eq(0);
+			for (Element e : elements2) {
+				String name = e.getElementsByTag("td").eq(0).text();
+				String tel = e.getElementsByTag("td").eq(2).text();
+				String add = e.getElementsByTag("td").eq(4).eq(0).toggleClass("listlink").text();
+//				System.out.println(name + "," + tel + "," + add.substring(0, add.lastIndexOf("大きな地図を見る 周辺のお店を探す")));
+				list.add(name + "," + tel + "," + add.substring(0, add.lastIndexOf("大きな地図を見る 周辺のお店を探す")));
+			}
+		}
+		Jdbc jdbc = new Jdbc();
+		try {
+			jdbc.getDb();
+
+			for(String s : list) {
+				String[] s2 = s.split(",");
+				Shop sp = new Shop();
+				sp.setName(s2[0]);
+				sp.setTel(s2[1]);
+				sp.setAddress(s2[2]);
+
+				jdbc.insertShopDate(sp);
+			}
+
+		} catch (ClassNotFoundException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
 		}
 	}
 }
